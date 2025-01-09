5f6e6d69069a420a5033700f472fdd09b67144b7
Siddhesh Lakhani <lakhani.siddhesh@gmail.com>
2025-01-09 11:57:33 +0530

    Updated layout and removed unused svgs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import Hero from '@/components/Hero'
 import React from 'react'
 
 export default function Home() {
   return (
-    <div className='h-screen w-screen flex justify-center items-center bg-primary text-7xl font-serif'>
-      Hi, I am Ami Lakhani!
-    </div>
+    <>
+      <Hero />
+    </>
   )
 }
---
+++ src/components/Hero.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div className='h-screen w-screen flex justify-center items-center bg-primary text-7xl font-serif'>
+      Hi, I am Dr. Ami Lakhani!
+    </div>
+  )
+}
